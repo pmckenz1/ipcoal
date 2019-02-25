53877569d9119bd86dc6c8cff850e31f8938fa5a
Patrick McKenzie <p.mckenzie@columbia.edu>
2019-02-25 17:12:19 -0500

    added package template, changed directory structure

---
+++ physeqs/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+__version__ = "0.2.1-dev"
+__author__ = "Patrick McKenzie"
+
+from .Coalseq import Coalseq
+from .Window import SlidingWindow
+from .MCMC import MCMC, Compare
+# from .Window import MBmcmc
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+setup(
+    name="strange",
+    packages=["strange"],
+    version="0.1",
+    author="Patrick McKenzie",
+    author_email="p.mckenzie@columbia.edu",
+    install_requires=[
+        "scipy>0.10",
+        "numpy>=1.9",
+        "pandas>=0.16",
+        "toytree",
+        "toyplot",
+    ],
+    license='GPL',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+)
