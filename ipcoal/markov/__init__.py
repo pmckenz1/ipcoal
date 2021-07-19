b45bde84f424ebba25e0f50adc051669bfdb8547
Deren <de2356@columbia.edu>
2021-07-19 10:55:56 -0400

    deprecate markov submodule

--- ipcoal/markov/__init__.py
+++ ipcoal/markov/__init__.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 """
+DEPRECATED IN PLACE OF SIM_MUTATIONS
+
 Markov substitution models for converting substitutions to bases
-under a defined model using JIT code.
+under a defined model using JIT code. 
+
+This has now been replaced by Markov models implemented directly
+in msprime as of v.1.0+.
 """
 
-from . import SeqModel
+# from . import SeqModel
